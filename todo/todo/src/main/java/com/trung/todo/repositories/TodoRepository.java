980e30ec86bae777c63d37076433ca0b4b38c306
Trung <trungpnt0605@gmail.com>
2021-06-16 18:59:55 +0700

    excluded data source driver finder

--- todo/todo/src/main/java/com/trung/todo/repositories/TodoRepository.java
+++ todo/todo/src/main/java/com/trung/todo/repositories/TodoRepository.java
@@ -2,9 +2,11 @@ package com.trung.todo.repositories;
 
 import com.trung.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     List<Todo> findTodoByCategoryId(Long categoryId);
