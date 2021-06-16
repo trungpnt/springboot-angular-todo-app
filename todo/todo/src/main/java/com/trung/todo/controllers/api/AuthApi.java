263e1ebe89e48a9b047c60e584eb3e270c0a5f01
Trung <trungpnt0605@gmail.com>
2021-06-16 16:33:48 +0700

    add api interface and related controller

---
+++ todo/todo/src/main/java/com/trung/todo/controllers/api/AuthApi.java
@@ -0,0 +1,22 @@
+package com.trung.todo.controllers.api;
+
+import com.trung.todo.dto.UserDto;
+import io.swagger.annotations.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static com.trung.todo.utils.Constants.APP_ROOT;
+
+@Api("authApi")
+public interface AuthApi {
+    @ApiOperation(value = "Login user", notes = "Creates a new user ", response = UserDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "The connected user.")
+    })
+    @PostMapping(value = APP_ROOT + "/auth/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<UserDto> loginUser(
+            @ApiParam(value = "User DTO", required = true) @RequestBody UserDto user
+    );
+}
