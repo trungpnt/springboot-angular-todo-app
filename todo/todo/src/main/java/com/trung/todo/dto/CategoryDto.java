008959130c63453ec4fc7af52e9c66025ede17c6
Trung <trungpnt0605@gmail.com>
2021-06-16 14:27:28 +0700

    added TodoDTo and CateogoryDto

--- todo/todo/src/main/java/com/trung/todo/dto/CategoryDto.java
+++ todo/todo/src/main/java/com/trung/todo/dto/CategoryDto.java
@@ -1,10 +1,13 @@
 package com.trung.todo.dto;
 
 
+import com.trung.todo.model.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -21,5 +24,25 @@ public class CategoryDto {
 
     private List<TodoDto> todoDtoList;
 
+    public static Category toEntity(CategoryDto categoryDto) {
+        Category category = new Category();
 
+        category.setUser(UserDto.toEntity(categoryDto.getUser()));
+        category.setId(categoryDto.getId());
+        category.setName(categoryDto.getName());
+        category.setDescription(categoryDto.getDescription());
+        
+        return category;
+    }
+
+    public static CategoryDto fromEntity(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .description(category.getDescription())
+                .todoList(
+                        category.getTodoList() != null ? category.getTodoList().stream().map(TodoDto::fromEntity).collect(Collectors.toList()) : null
+                )
+                .build();
+    }
 }
---
+++ todo/todo/src/main/java/com/trung/todo/dto/TodoDto.java
@@ -0,0 +1,54 @@
+package com.trung.todo.dto;
+
+
+import com.trung.todo.model.Todo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TodoDto {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private ZonedDateTime startDate;
+
+    private boolean done;
+
+    private boolean favorite;
+
+    private CategoryDto categoryDto;
+
+    public static Todo toEntity(TodoDto todoDto) {
+        final Todo todo = new Todo();
+        todo.setId(todoDto.getId());
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setDone(todoDto.isDone());
+        todo.setFavorite(todoDto.isFavorite());
+        todo.setStartDate(ZonedDateTime.now());
+        todo.setCategory(CategoryDto.toEntity(todoDto.getCategory()));
+
+        return todo;
+    }
+
+    public static TodoDto fromEntity(Todo todo) {
+        return TodoDto.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .description(todo.getDescription())
+                .startDate(todo.getStartDate())
+                .done(todo.isDone())
+                .favorite(todo.isFavorite())
+                .build();
+    }
+}
