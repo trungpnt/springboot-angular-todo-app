0bbf3104ff022d96820337753d5b16f7efc0147a
Trung <trungpnt0605@gmail.com>
2021-06-16 15:41:38 +0700

    added user and category repo

---
+++ todo/todo/src/main/java/com/trung/todo/handlers/ErrorDto.java
@@ -0,0 +1,14 @@
+package com.trung.todo.handlers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ErrorDto {
+}
---
+++ todo/todo/src/main/java/com/trung/todo/repositories/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.trung.todo.repositories;
+
+import com.trung.todo.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+    List<Category> findCategoryByUserId(Long userId);
+
+    //if this nativeQuery = true was added then raw queries
+    @Query( "select t.category.id from Todo t where t.id = :todoId")
+    Long findCategoryByTodoId(@Param("todoId") Long todoId);
+
+    @Query("select c from Category c inner join Todo t on t.category.id = c.id  where t.startDate >= :startDate and t.startDate <= :endDate and c.user.id = :userId ")
+    List<Category> getAllTodoByCategoriesForToday(@Param("startDate")ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate,@Param ("userId") Long userId);
+}
