56402acc54a1f8ecde72cf2489ae9f7d1c44c828
Trung <trungpnt0605@gmail.com>
2021-06-16 16:14:39 +0700

    added & modified swagger config

---
+++ todo/todo/src/main/java/com/trung/todo/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.trung.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static com.trung.todo.utils.Constants.APP_ROOT;
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket (DocumentationType.SWAGGER_2)
+                .apiInfo(
+                        new ApiInfoBuilder().description("")
+                            .title("To do list Api documentation")
+                            .version("1.0")
+                            .description("Api Documentation")
+                            .build()
+                )
+                .groupName("REST API V1")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.trung.todo"))
+                .paths(PathSelectors.ant(APP_ROOT + "/**"))
+                .build();
+    }
+}
